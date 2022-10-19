fdeb7fbf60d8e3ee413e1a9c65d37d52e8d08926
Andrei Pivavaruk <259220@student.pwr.edu.pl>
2022-10-19 09:58:20 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QDialog
+from ui_imagedialog import Ui_ImageDialog
+
+app = QApplication(sys.argv)
+window = QDialog()
+ui = Ui_ImageDialog()
+ui.setupUi(window)
+
+window.show()
+sys.exit(app.exec())
+
+
